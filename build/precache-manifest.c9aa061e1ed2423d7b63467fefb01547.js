ccdc06084542e277630a6725025aa31a0481f8c0
jchzhang <44760954+jchzhang@users.noreply.github.com>
2019-10-25 21:08:50 +0800

    1

---
+++ build/precache-manifest.c9aa061e1ed2423d7b63467fefb01547.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4be59e314cb633bfb8e85e9d106a37e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6820a279ca99b41e6f67ec217b5e13fe",
+    "url": "/order.html"
+  },
+  {
+    "revision": "5b5abe034af9b0cda5492a52bcb43750",
+    "url": "/query.html"
+  },
+  {
+    "revision": "4e867f18b6da90f468ba",
+    "url": "/static/css/0.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f41e0656a1bfb8cb492a",
+    "url": "/static/css/index.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8e2889eaf6bfcb081d1a",
+    "url": "/static/css/order.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e40687cf2fdbbf11f219",
+    "url": "/static/css/query.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0ea8ddf3e65ae8c7e9e2",
+    "url": "/static/css/ticket.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4e867f18b6da90f468ba",
+    "url": "/static/js/0.4fc4f630.chunk.js"
+  },
+  {
+    "revision": "f41e0656a1bfb8cb492a",
+    "url": "/static/js/index.a7752551.chunk.js"
+  },
+  {
+    "revision": "8e2889eaf6bfcb081d1a",
+    "url": "/static/js/order.943d3ff5.chunk.js"
+  },
+  {
+    "revision": "e40687cf2fdbbf11f219",
+    "url": "/static/js/query.73caa08b.chunk.js"
+  },
+  {
+    "revision": "842d422171ba5f4e0704",
+    "url": "/static/js/runtime-index.c13bf65f.js"
+  },
+  {
+    "revision": "37439dd083022ae9b8a0",
+    "url": "/static/js/runtime-order.0e6ed3c6.js"
+  },
+  {
+    "revision": "23dfd80ecb0d25744cf5",
+    "url": "/static/js/runtime-query.6943cb09.js"
+  },
+  {
+    "revision": "96852abcddc86e9e8d26",
+    "url": "/static/js/runtime-ticket.e15abed8.js"
+  },
+  {
+    "revision": "0ea8ddf3e65ae8c7e9e2",
+    "url": "/static/js/ticket.3f6c320e.chunk.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "34f8c3a520a2d015b398ef8ec1b08788",
+    "url": "/ticket.html"
+  }
+]);
